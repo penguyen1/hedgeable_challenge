8596a99de97a32c9ee56ab6d4b929a89ca094f4d
penguyen <peternguyen008@gmail.com>
2016-05-12 22:55:44 -0400

    initializing connection to server

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+'use strict'
+const express     = require('express');
+const logger      = require('morgan');        // logs request details
+const path        = require('path');          // defines API route paths
+const bodyParser  = require('body-parser');   // parses user input into JSON
+const app         = express();
+
+// const secret = process.env.SECRET;
+// const expressJWT = require('express-jwt');     // user auth - necessary?
+// const jwt = require('jsonwebtoken');
+
+const userRoutes = require( path.join(__dirname, '/routes/users'));
+app.use('/users', userRoutes);
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, '/public')));    // index.html
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// HOMEPAGE
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Hellooo Handsome!! ', port);
+});
