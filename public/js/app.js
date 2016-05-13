c7ee881e8e48003453cceaa461782b7fe246f47b
penguyen <peternguyen008@gmail.com>
2016-05-13 08:52:26 -0400

    attempting to render data to app component

--- public/js/app.js
+++ public/js/app.js
@@ -22,16 +22,18 @@ var callback = function(error, data, response) {
 const App = React.createClass({
   getInitialState() {
     return {
-      client: {},
       token: "",
+      usertoken: "",
+      clientId: "",
       portfolio: {}
     }
   },
 
   // data to be passed to child components
   childContextTypes: {
-    client: React.PropTypes.object,
-    token: React.PropTypes.string
+    token: React.PropTypes.string,
+    usertoken: React.PropTypes.string,
+    portfolio: React.PropTypes.object
   },
 
   componentWillMount(){
@@ -42,49 +44,59 @@ const App = React.createClass({
       dataType: 'json'
     }).done((data) => {
       // console.log('getting API Partner token: ', data)
-      this.setState({ token: data.response })     // API Partner authorization token
+      this.setState({ token: data.response })     // API Partner authorization {token}
 
-      // retrieves all account types
+      // // retrieves all account types
+      // $.ajax({
+      //   url: `https://api.hedgeable.com/accounttypes?token=${this.state.token}`,
+      //   type: 'GET',
+      //   dataType: 'json'
+      // }).done((data) => {
+      //   console.log('getting all account types??: ', data)
+      // })
+
+      // authenticates client & gets client {usertoken}
       $.ajax({
-        url: `https://api.hedgeable.com/accounttypes?token=${token}`,
-        type: 'GET',
+        url: `https://api.hedgeable.com/client/login?token=${this.state.token}&email=apidemo@hedgeable.com&password=gethedged`,
+        type: 'POST',
         dataType: 'json'
       }).done((data) => {
-        console.log('getting all account types??: ', data)
+        // console.log('{usertoken} is: ', data.response)   
+        this.setState({ 
+          usertoken: data.response.token,   // authenticated client {usertoken}
+          clientId: data.response.id        // clientId 
+        }) 
 
-        // authenticates client & gets client {usertoken}
+        // retrieves all client account info
         $.ajax({
-          url: `https://api.hedgeable.com/client/login?token=${this.state.token}&email=apidemo@hedgeable.com&password=gethedged`,
-          type: 'POST',
+          url: `https://api.hedgeable.com/client/${this.state.clientId}/getallinformation?token=${this.state.token}&usertoken=${this.state.usertoken}`,
+          type: 'GET',
           dataType: 'json'
         }).done((data) => {
-          // console.log('{usertoken} is: ', data.response)   
-          this.setState({ client: data.response }) 
-          // {usertoken}: data.response.token
-          // clientid: data.response.id
-
-            // retrieves all client account info        17379 - 17978
-            $.ajax({
-              url: `https://api.hedgeable.com/client/${this.state.client.id}/getallinformation?token=${this.state.token}&usertoken=${this.state.client.token}`,
-              type: 'GET',
-              dataType: 'json'
-            }).done((data) => {
-              // console.log('entire client portfolio: ', data.response)
-              this.setState({ portfolio: data.response })
-              console.log('entire client portfolio: ', this.state)
-            });
-
-        })
-
-      })
-
+          // console.log('entire client portfolio: ', data.response)
+          this.setState({ portfolio: data.response })
+          console.log('this.state: ', this.state)
+        });
+      });
     });
   },
 
   render() {
     return (
-      <div id="page_bg">
-        React works!
+      <div id="container-bg">
+        <div id="upperInfo">
+          <h1>Hello, {this.state.portfolio.length ? this.state.portfolio.client.firstName : "Guest"}!</h1>
+          { this.state.portfolio.length 
+            ? (<h3>Current portfolio balance: ${this.state.portfolio.latestBalance.toLocaleString()}</h3>)
+            : console.log('portfolio does not exist yet') }
+          { this.state.portfolio.length 
+            ? (<h4>Hedged Percentage: {this.state.portfolio.hedgedPercentage.toFixed(3)}%</h4>)
+            : console.log('portfolio does not exist yet') }
+              
+        </div>
+        <div id="lowerInfo">
+          input accountHoldingsTable & accountGrowthChart here
+        </div>
       </div>
     )
   }
